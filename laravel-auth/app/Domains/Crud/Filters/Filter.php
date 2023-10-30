5236b46b3fcaebef5fc124fe73d901a322891707
Robert B <robert@planetweb.ro>
2023-10-30 16:32:47 +0200

    'v1.0.0'

--- laravel-auth/app/Domains/Crud/Filters/Filter.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Domains\Crud\Filters;
-
-use Lumi\Core\Filter as Lumi_Filter;
-
-class Filter extends Lumi_Filter
-{
-    protected static $table = 'crud';
-}
--- laravel/app/Domains/Crud/Presenters/Presenter.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Domains\Crud\Presenters;
-
-class Presenter
-{
-    public static function run($item) {
-        return $item;
-    }
-}
