7c052a7081c5b11eb8fb2d0f857fe1d717001dec
Robert B <robert@planetweb.ro>
2024-06-04 00:37:20 +0300

    'v1.0.1'

--- laravel-auth/app/Domains/Crud/Presenters/ListPresenter.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Domains\Crud\Presenters;
-
-class ListPresenter
-{
-    public static function run($items) {
-        return $items;
-    }
-}
--- laravel/app/Models/Crud.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-
-class Crud extends Model
-{
-    protected $table = 'crud';
-}
